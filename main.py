0e51713da5fcc0693b503dc13d5a25f919ba808c
Rafael <rafael.souza@hotmart.com>
2024-02-24 11:56:54 -0300

    main menu refactor.

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ from graphs import product_type_graph, find_parent_purchases, countries_top_sale
 if __name__ == '__main__':
     csv_file_path = '/Users/rafael.souza/Desktop/case dados/drive-download-20240220T202251Z-001/Purchases.csv'
 
-    print("Available graphs")
-    print("1 - Product type \n2 - Countries with the most sales"
-          "\n3 - Countries with the most subscriptions"
-          "\n4 - Most used recurrences"
-          "\n0 - Exit")
+    while True:
+        print("Available graphs")
+        print("1 - Product type \n2 - Countries with the most sales"
+              "\n3 - Countries with the most subscriptions"
+              "\n4 - Most used recurrences"
+              "\n0 - Exit")
 
-    graph_chosen = input("What graph would you like to see: ")
-    if graph_chosen == "1":
-        product_type_graph(csv_file_path)
-    elif graph_chosen == "2":
-        countries_top_sales(csv_file_path)
-    elif graph_chosen == "3":
-        countries_top_subs(csv_file_path)
-    elif graph_chosen == "4":
-        most_used_recurrences(csv_file_path)
+        graph_chosen = input("What graph would you like to see: ")
+
+        if graph_chosen == "0":
+            break
+
+        if graph_chosen == "1":
+            product_type_graph(csv_file_path)
+        elif graph_chosen == "2":
+            countries_top_sales(csv_file_path)
+        elif graph_chosen == "3":
+            countries_top_subs(csv_file_path)
+        elif graph_chosen == "4":
+            most_used_recurrences(csv_file_path)
+        else:
+            print("Invalid option.")
 
 
