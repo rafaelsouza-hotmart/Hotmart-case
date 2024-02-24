3a6c2bca566de91532873a1cc58d9b90bc2b23be
Rafael <rafael.souza@hotmart.com>
2024-02-24 11:32:12 -0300

    top sales by country

--- graphs.py
+++ graphs.py
@@ -17,3 +17,33 @@ def product_type_graph(csv_file_path):
     plt.tight_layout()
 
     plt.show()
+
+def find_parent_purchases(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+
+    filtered_df = df[df['purchase_parent_id'] == "97-124-26-127-125-126-112-28-116"]
+
+    filtered_df = filtered_df[['purchase_parent_id', 'purchase_installment_number']]
+
+    return filtered_df
+
+def countries_top_sales(csv_file_path):
+    print("Loading top sales by country...")
+    df = pd.read_csv(csv_file_path)
+
+    #sales
+    original_sales = df[df['purchase_parent_id'].isnull()]
+
+    #countries
+    country_count = original_sales['user_buyer_country'].value_counts()
+
+    plt.figure(figsize=(12, 6))
+    country_count.plot(kind='bar', color='skyblue')
+    plt.title('Number of Sales by Country (Original Purchases)')
+    plt.xlabel('Country')
+    plt.ylabel('Number of Sales')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+
+    plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from graphs import product_type_graph
+from graphs import product_type_graph, find_parent_purchases, countries_top_sales
 
 if __name__ == '__main__':
     csv_file_path = '/Users/rafael.souza/Desktop/case dados/drive-download-20240220T202251Z-001/Purchases.csv'
 
     print("Available graphs")
-    print("1 - Product type \n2 - Sale Amount")
+    print("1 - Product type \n2 - Countries with the most sales"
+          "\n3 - Countries with the most subscriptions"
+          "\n0 - Exit")
 
     graph_chosen = input("What graph would you like to see: ")
-
     if graph_chosen == "1":
         product_type_graph(csv_file_path)
+    elif graph_chosen == "2":
+        countries_top_sales(csv_file_path)
 
 
